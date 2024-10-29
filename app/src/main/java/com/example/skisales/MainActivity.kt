577aa939dcd38f8e905f922c0a3dc83541b7732d
Hin <lopyx191@gmail.com>
2024-10-29 11:46:14 +0700

    add main disp + database

--- app/src/main/java/com/example/skisales/MainActivity.kt
+++ app/src/main/java/com/example/skisales/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.skisales
 import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
+import com.squareup.picasso.Picasso
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,27 +25,58 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         val db = Firebase.firestore // init db
-
-
-
-        // Create a new user with a first and last name
-        val user = hashMapOf(
-            "first" to "Mixa",
-            "last" to "Demon",
-            "born" to 2006,
+        val layout: LinearLayout = findViewById(R.id.linlay_1)
+        val layout1: LinearLayout = findViewById(R.id.linlay_2)
+        val layout2: LinearLayout = findViewById(R.id.linlay_3)
+        val layout3: LinearLayout = findViewById(R.id.linlay_4)
+        val layout4: LinearLayout = findViewById(R.id.linlay_5)
+        val layout5: LinearLayout = findViewById(R.id.linlay_6)
+        var params = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            1.0f
         )
 
-        // Add a new document with a generated ID
-        db.collection("users")
-            .add(user)
-            .addOnSuccessListener { documentReference ->
-                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
-                println("Zebumba!")
+
+        db.collection("inv")
+            .get()
+            .addOnSuccessListener { result ->
+                Log.d(TAG,"${result.size()}")
+                var counter = 0
+                var layout_path_img = layout
+                var layout_path_names = layout1
+                for (document in result) {
+                    counter++
+                    if (counter > 3) {
+                        layout_path_img = layout2
+                        layout_path_names = layout3
+                        if (counter > 6) {
+                            layout_path_img = layout4
+                            layout_path_names = layout5
+                        }
+                    }
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                    Log.d(TAG,"${document.data.get("name")}")
+                    val imageView = ImageView(this)
+                    Picasso.get().load("${document.data.get("img")}").into(imageView)
+                    imageView.adjustViewBounds = true
+                    imageView.setLayoutParams(params)
+                    layout_path_img.addView(imageView)
+                    val textView = TextView(this)
+                    textView.text = "${document.data.get("name")}"
+                    textView.textSize = 15f
+                    textView.setLayoutParams(params)
+                    layout_path_names.addView(textView)
+
+                }
             }
-            .addOnFailureListener { e ->
-                Log.w(TAG, "Error adding document", e)
-                println("Megafail!")
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
             }
+
     }
+
+
 }
